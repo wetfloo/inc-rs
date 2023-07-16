aa8b2f02ff0656cff2a37876e581312f7f4c1f2f
wetfloo <yahahaha.1234567890@gmail.com>
2023-07-16 13:43:40 +0700

    Basics for Clone, Copy and Default

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,29 @@
+#![allow(unused, dead_code)]
+
 fn main() {
-    println!("Implement me!");
+    let point = Point::default();
+    let line = Polyline(Vec::new()); // empty line
+
+    takes_point(point); // copied implicitly
+    takes_point(point); // copied implicitly
+
+    // Wouldn't compile, line isn't Copy
+    // takes_line(line);
+    // takes_line(line);
+
+    takes_line(line.clone());
+    takes_line(line.clone());
 }
+
+#[derive(Default, Clone, Copy)]
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Clone)]
+struct Polyline(Vec<Point>);
+
+fn takes_point(point: Point) {}
+
+fn takes_line(line: Polyline) {}
