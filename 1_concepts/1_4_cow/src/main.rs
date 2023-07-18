ee0f41ad897219e8244a624537af230fd0ffcbd5
wetfloo <yahahaha.1234567890@gmail.com>
2023-07-18 21:58:14 +0700

    Lazily evaluate CLI args using Cow

--- 1_concepts/1_4_cow/src/main.rs
+++ 1_concepts/1_4_cow/src/main.rs
@@ -1,3 +1,27 @@
+use std::{borrow::Cow, env};
+
 fn main() {
-    println!("Implement me!");
+    let arg = get_conf_arg()
+        .or_else(get_env_arg)
+        .unwrap_or_else(|| Cow::Borrowed("/etc/app/app.conf"));
+
+    println!("{}", arg);
 }
+
+fn get_conf_arg() -> Option<ArgCow> {
+    let mut args = env::args().skip(1);
+    let conf_key = args.next()?;
+    let conf_value = args.next()?;
+
+    match conf_key.as_str() {
+        "--conf" => Some(Cow::Owned(conf_value)),
+        _ => None,
+    }
+}
+
+fn get_env_arg() -> Option<ArgCow> {
+    let res = env::var("APP_CONF").ok()?;
+    Some(Cow::Owned(res))
+}
+
+type ArgCow = Cow<'static, str>;
